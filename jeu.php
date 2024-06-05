ac0ec597298fb1d9ba7f024ca7758efcdff5d152
JessyTS <jessymbuyi22@gmail.com>
2024-06-05 20:12:51 +0100

    modified:   jeu.php

--- jeu.php
+++ jeu.php
@@ -3,14 +3,16 @@
     require 'header.php'
 ?>
 
-<?php
-    if ($_GET['chiffre'] > $devine):
-        echo "Votre nombre est trop grand !!"
-    endif
-?>
+<?php if ($_GET['chiffre'] > $devine): ?>
+    Votre chiffre est trop grand !!
+<?php elseif ($_GET['chiffre'] < $devine): ?>
+    Votre chiffre est trop petit
+<?php else: ?>
+    Bravo vous avez deviner le chiffre !!
+<?php endif ?>
 
 <form action="/jeu.php" method="GET">
-    <input type="number" name="chiffre" placeholder="Entre 0 et 1000">
+    <input type="number" name="chiffre" placeholder="Entre 0 et 1000" value="<?= $_GET['chiffre'] ?>">
     <button type="submit">Deviner</button>
 </form>
 
